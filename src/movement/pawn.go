a03a678396f57b80f12318d62816b40db5143b09
Pianomyn <anditan2014@gmail.com>
2026-03-19 19:34:07 +1100

    pawn movement and basic tests

--- src/movement/pawn.go
+++ src/movement/pawn.go
@@ -1,24 +1,60 @@
 package movement
 
-import "chessgo/board"
+import (
+	"chessgo/board"
+	"chessgo/movement/movement_utility"
+)
 
-func GetPawnMoves() []board.Bitboard {
-	pushes := GetPawnPushes()
-	attacks := GetPawnAttacks()
-	moves := make([]board.Bitboard, 64)
-	for i := range pushes {
-		moves[i] = pushes[i] | attacks[i]
-	}
-	return moves
-}
-
-func GetPawnPushes() []board.Bitboard {
+func GetPawnPushes(side board.Side) []board.Bitboard {
 	pushes := make([]board.Bitboard, 64)
+	for i := range pushes {
+		currentBit := board.Bitboard(1) << i
+		if side == board.White {
+			if currentBit&(board.Rank1Mask|board.Rank8Mask) != 0 {
+				continue
+			} else if currentBit&board.Rank2Mask != 0 {
+				pushes[i] |= currentBit<<movement_utility.N | currentBit<<movement_utility.N*2
+			} else {
+				pushes[i] |= currentBit << movement_utility.N
+			}
+		} else {
+			if currentBit&(board.Rank1Mask|board.Rank8Mask) != 0 {
+				continue
+			} else if currentBit&board.Rank7Mask != 0 {
+				pushes[i] |= currentBit>>-movement_utility.S | currentBit>>-movement_utility.S*2
+			} else {
+				pushes[i] |= currentBit >> -movement_utility.S
+			}
+		}
+	}
 	return pushes
-
 }
 
-func GetPawnAttacks() []board.Bitboard {
+func GetPawnAttacks(side board.Side) []board.Bitboard {
 	attacks := make([]board.Bitboard, 64)
+	for i := range attacks {
+		currentBit := board.Bitboard(1) << i
+		if side == board.White {
+			if currentBit&(board.Rank1Mask|board.Rank8Mask) != 0 {
+				continue
+			} else if currentBit&board.FileAMask != 0 {
+				attacks[i] |= currentBit << movement_utility.NE
+			} else if currentBit&board.FileHMask != 0 {
+				attacks[i] |= currentBit << movement_utility.NW
+			} else {
+				attacks[i] |= currentBit<<movement_utility.NE | currentBit<<movement_utility.NW
+			}
+		} else {
+			if currentBit&(board.Rank1Mask|board.Rank8Mask) != 0 {
+				continue
+			} else if currentBit&board.FileAMask != 0 {
+				attacks[i] |= currentBit >> -movement_utility.SE
+			} else if currentBit&board.FileHMask != 0 {
+				attacks[i] |= currentBit >> -movement_utility.SW
+			} else {
+				attacks[i] |= currentBit>>-movement_utility.SE | currentBit>>-movement_utility.SW
+			}
+		}
+	}
 	return attacks
 }
--- src/movement/pawn_test.go
+++ src/movement/pawn_test.go
@@ -1,1 +1,72 @@
 package movement
+
+import (
+	"chessgo/board"
+	"math/bits"
+	"testing"
+)
+
+func TestPawnPushCounts(t *testing.T) {
+	whitePushes := GetPawnPushes(board.White)
+	blackPushes := GetPawnPushes(board.Black)
+
+	tests := []struct {
+		name     string
+		table    []board.Bitboard
+		square   board.Square
+		expected int
+	}{
+		// White Pawns
+		{"A2", whitePushes, board.A2, 2},
+		{"D4", whitePushes, board.D4, 1},
+		{"H7", whitePushes, board.H7, 1},
+
+		// Black Pawns
+		{"E7", blackPushes, board.E7, 2},
+		{"E6", blackPushes, board.E6, 1},
+		{"A2", blackPushes, board.A2, 1},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := bits.OnesCount64(uint64(tt.table[tt.square]))
+			if got != tt.expected {
+				t.Errorf("%s: expected %d pushes, got %d", tt.name, tt.expected, got)
+			}
+		})
+	}
+}
+
+func TestPawnAttackCounts(t *testing.T) {
+	whiteTable := GetPawnAttacks(board.White)
+	blackTable := GetPawnAttacks(board.Black)
+
+	tests := []struct {
+		name     string
+		table    []board.Bitboard
+		square   board.Square
+		expected int
+	}{
+		{"A2", whiteTable, board.A2, 1},
+		{"H2", whiteTable, board.H2, 1},
+		{"D2", whiteTable, board.D2, 2},
+		{"D4", whiteTable, board.D4, 2},
+
+		{"A7", blackTable, board.A7, 1},
+		{"H7", blackTable, board.H7, 1},
+		{"D7", blackTable, board.D7, 2},
+		{"D5", blackTable, board.D5, 2},
+
+		{"White_A8_Invalid", whiteTable, board.A8, 0},
+		{"Black_A1_Invalid", blackTable, board.A1, 0},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := bits.OnesCount64(uint64(tt.table[tt.square]))
+			if got != tt.expected {
+				t.Errorf("Square %v: expected %d attacks, got %d", tt.square, tt.expected, got)
+			}
+		})
+	}
+}
