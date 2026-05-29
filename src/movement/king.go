38ffb991a2a5d9c76f31845883e5246a58b77eb8
Pianomyn <anditan2014@gmail.com>
2026-05-29 16:04:55 +1000

    king moves attacktable moves, todo castling and tests

--- src/movement/king.go
+++ src/movement/king.go
@@ -3,7 +3,48 @@ package movement
 import (
 	"chessgo/board"
 	"chessgo/movement/movement_utility"
+	"math/bits"
 )
+
+func GetKingMoves(cb *board.ChessBoard) []movement_utility.Move {
+	attackTable := GetKingAttackTable()
+	var moves []movement_utility.Move
+
+	source := board.Square(bits.TrailingZeros64(uint64(cb.Pieces[cb.SideToMove][board.King])))
+	targets := attackTable[source] &^ cb.Colours[cb.SideToMove]
+	colouredKing := board.PieceToColouredPiece(board.King, cb.SideToMove)
+
+	for targets != 0 {
+		target := board.GetNextPieceSquare(&targets)
+		targetPiece := cb.Mailbox[target]
+		moves = append(moves, movement_utility.Move{
+			Source:        source,
+			Target:        target,
+			SourcePiece:   colouredKing,
+			CapturedPiece: targetPiece,
+			Flags:         movement_utility.MoveFlags{},
+		})
+	}
+
+	// Castling
+	if cb.SideToMove == board.White {
+		if cb.CastlingRights.WhiteKingside {
+
+		}
+		if cb.CastlingRights.WhiteQueenside {
+
+		}
+	} else {
+		if cb.CastlingRights.BlackKingside {
+
+		}
+		if cb.CastlingRights.BlackQueenside {
+
+		}
+	}
+
+	return moves
+}
 
 func GetKingAttackTable() []board.Bitboard {
 	kingAttacks := make([]board.Bitboard, 64)
